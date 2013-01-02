8b962bb85bee658c9e4190099b0a6f815d997057
think-free <meumeu1402@gmail.com>
2013-01-02 20:16:46 +0100

    Update rpi-Install-BaseDependencies.sh

--- rpi-Install-BaseDependencies.sh
+++ rpi-Install-BaseDependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/think-free/pi-packages/raw/master/nodejs_0.8.15-1_armhf.deb
+wget https://github.com/think-free/pi-packages/raw/master/nodejs_0.8.15-1_armhf.deb --no-check-certificate
 
 dpkg -i nodejs_0.8.15-1_armhf.deb
 
