187c2d569fc56587ab5d8531931ba2c2cd4dfc62
christophe <christophe@Naboo.(none)>
2013-01-05 14:11:18 +0100

    rpi-MakeImage

--- rpi-MakeImage.sh
+++ rpi-MakeImage.sh
@@ -2,9 +2,9 @@
 
 DATE=`date +%Y-%m-%d-at-%H-%M-%S`
 
-sudo dd if=/dev/$1 of=Ownode-RPiImage-$DATE.img
-# mkdir mnt 
-# sudo mount -o loop,offset=$((512*122880)) Ownode-RPiImage-$DATE.img mnt 
-# sudo sfill -z -l -l -f mnt 
-# sudo umount mnt
-# sudo rm -rf mnt
+sudo dd if=/dev/$1 of=Ownode-RPiImage-$DATE.img bs=1M count=1894400
+ mkdir mnt 
+ sudo mount -o loop,offset=$((512*122880)) Ownode-RPiImage-$DATE.img mnt 
+ sudo sfill -z -l -l -f mnt 
+ sudo umount mnt
+ sudo rm -rf mnt
